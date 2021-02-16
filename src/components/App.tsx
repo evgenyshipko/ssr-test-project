c11403eb441cc0d60b16a29ce31d8daa309c7314
shipko <evgeny-shipko@yandex.ru>
2021-02-16 23:11:59 +0700

    some changes in webpack settings, add eslint, prettier, editorconfig

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,21 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 import 'src/styles/App.css';
 
 class App extends Component {
-
     state = {
-        color: 'blue'
-    }
+        color: 'blue',
+    };
 
     toggleColor = () => {
-        const color = this.state.color === 'blue' ? 'red' : 'blue'
-        this.setState({color})
-    }
+        const color = this.state.color === 'blue' ? 'red' : 'blue';
+        this.setState({ color });
+    };
 
     render() {
         return (
             <div>
-                <h1 style={{color: this.state.color}}>Example component</h1>
+                <h1 style={{ color: this.state.color }}>Example component</h1>
                 <button onClick={this.toggleColor}>Click me!</button>
             </div>
         );
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,38 +1,55 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const path = require('path');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
 
 module.exports = {
-    entry: "./src/index.tsx",
+    entry: './src/index.tsx',
     output: {
-        path: path.join(__dirname, "../dist"),
-        filename: "bundle.js"
+        publicPath: '/',
+        path: path.join(__dirname, '../dist'),
+        filename: 'bundle.js',
     },
     resolve: {
-        extensions: ['.tsx','.ts','.js'],
+        extensions: ['.tsx', '.ts', '.js'],
         alias: {
-            src: path.resolve(__dirname, '../src/')
-        }
+            '@src': path.resolve(__dirname, '../src/'),
+            '@components': path.resolve(__dirname, '../src/components'),
+        },
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/
+                use: 'ts-loader',
+                exclude: /node_modules/,
             },
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
-            }
-        ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: 'file-loader',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                loader: 'file-loader',
+            },
+        ],
     },
     plugins: [
         new MiniCssExtractPlugin(),
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: "./www/index.html"
-        })
-    ]
+            template: './public/index.html',
+            minify: {
+                collapseWhitespace: isProd,
+            },
+        }),
+    ],
 };
